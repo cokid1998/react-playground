75a06606ea5341ac681635da9a16ffb5f7efb798
이태관 <cokid98@naver.com>
2025-12-24 22:21:37 +0900

    tailwind setting

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>asdf</>;
+  return <div className="text-red-500">asdf</div>;
 }
 
 export default App;
