791e9690f0480b4a35ec6a9cf66d5ab8f5769d9c
이태관 <cokid98@naver.com>
2026-06-12 09:16:29 +0900

    객체 리터럴로 상태를 가져오면 발생하는 문제

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import useStore from "./store/Store";
 
+// 아래와 같이 상태를 가져오면 무한 루프 발생!
+// 원래 가지고 있던 상태의 주소와 다른 새로운 주소를 가진 객체 리터럴을 반환하기 때문
+
 function TokenComp() {
   /*
   // useStore를 전체 구독을 하기 때문에
@@ -16,19 +19,39 @@ function TokenComp() {
 
   /**
    * Selector함수는 Store의 상태값이 1개라도 변경되면 다시 실행된다.
+    const token = useStore((state) => {
+      console.log("1");
+      return state.token;
+    });
+    const login = useStore((state) => {
+      console.log("2");
+      return state.login;
+    });
+    const logout = useStore((state) => {
+      console.log("3");
+      return state.logout;
+    });
    */
-  const token = useStore((state) => {
-    console.log("1");
-    return state.token;
-  });
-  const login = useStore((state) => {
-    console.log("2");
-    return state.login;
-  });
-  const logout = useStore((state) => {
-    console.log("3");
-    return state.logout;
-  });
+
+  /**
+   * 아래 코드처럼 상태를 가져오면 무한 렌더링이 유발된다.
+   * 이유는 {token, login, logout}같은 새로운 리터럴 객체를 리턴하면
+   * 값은 똑같지만 이전상태와 참조값이 다르기 때문에 zustand는 값이 바뀌었다고 인식하고 리렌더링을 유발시킨다.
+   * 위 과정이 무한이 반복하여 문제가 발생!
+   * 해결법 1: 그냥 selector를 통해 3개를 따로 가져오기
+   * const token = useStore((state) => state.token);
+   * const login = useStore((state) => state.login);
+   * const logout = useStore((state) => state.logout);
+   *
+   * 해결법 2: useShallow사용
+   *
+   * 해결법 3: 커스텀 동등성 함수 사용
+   */
+  const { token, login, logout } = useStore((state) => ({
+    token: state.token,
+    login: state.login,
+    logout: state.logout,
+  }));
   console.log("토큰 컴포넌트 리렌더");
 
   return (
@@ -46,14 +69,14 @@ function TokenComp() {
 
 function ThemeComp() {
   // const { theme, setTheme } = useStore();
-  const theme = useStore((state) => {
-    console.log("4");
-    return state.theme;
-  });
-  const setTheme = useStore((state) => {
-    console.log("5");
-    return state.setTheme;
-  });
+
+  // const theme = useStore((state) => state.theme);
+  // const setTheme = useStore((state) => state.setTheme);
+
+  const { theme, setTheme } = useStore((state) => ({
+    theme: state.theme,
+    setTheme: state.setTheme,
+  }));
   console.log("테마 컴포넌트 리렌더");
 
   return (
@@ -68,14 +91,14 @@ function ThemeComp() {
 
 function UserComp() {
   // const { user, setName } = useStore();
-  const user = useStore((state) => {
-    console.log("6");
-    return state.user;
-  });
-  const setName = useStore((state) => {
-    console.log("7");
-    return state.setName;
-  });
+
+  // const user = useStore((state) => state.user);
+  // const setName = useStore((state) => state.setName);
+
+  const { user, setName } = useStore((state) => ({
+    user: state.user,
+    setName: state.setName,
+  }));
   console.log("유저 컴포넌트 리렌더");
 
   return (
